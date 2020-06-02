93f6dbbf7a65c34ab6474451d2ecb6c4dc5ebf08
Sven Strickroth <email@cs-ware.de>
2020-06-02 19:31:52 +0200

    Close handles

--- src/TGitCache/DirectoryWatcher.cpp
+++ src/TGitCache/DirectoryWatcher.cpp
@@ -1,4 +1,4 @@
-// TortoiseGit - a Windows shell extension for easy version control
+﻿// TortoiseGit - a Windows shell extension for easy version control
 
 // External Cache Copyright (C) 2005-2008, 2011-2012 - TortoiseSVN
 // Copyright (C) 2008-2017, 2019 - TortoiseGit
@@ -525,7 +525,10 @@ void CDirectoryWatcher::WorkerThread()
 						// wait a while. We don't want to have this thread
 						// running using 100% CPU if something goes completely
 						// wrong.
+						pdi->CloseDirectoryHandle();
+						watchedPaths.RemovePath(pdi->m_DirName);
 						Sleep(200);
+						CloseCompletionPort();
 					}
 				}
 			}
